3022e25dada4f2cd6a9df240e6be09b570bbb812
Darshan modi <modidarshan283@gmail.com>
2026-07-12 18:21:45 +0530

    Add FastAPI import and comment for future routes
    Added import statement for FastAPI and a comment about future routes.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from fastapi import FastAPI
 
 app = FastAPI(title="This is my task management application")
+#will create some routes in main to connect it with other files i will use for other purposes like user routes data bases and many more 
